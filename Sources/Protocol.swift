09af604dadd657ac383176c7ec43e638ca378c91
Rohit Agarwal <rohit.net@gmail.com>
2024-12-23 13:51:11 -0800

    Step 1 - Building a Protocol Handler

--- Sources/Protocol.swift
+++ Sources/Protocol.swift
@@ -1,1 +1,110 @@
 import Foundation
+
+struct Protocol {
+    func extractFrame(from buffer: Data) -> (RESPData, Int) {
+        let (payload, size) = readUntilCrlf(from: buffer)
+        guard let payload else {
+            return (RESPData.Null, 0)
+        }
+        
+        switch buffer[0].toChar() {
+        case "+": return (.SimpleString(payload.toString()), size)
+        case "-": return (.Error(payload.toString()), size)
+        case ":": return (.Integer(Int(payload.toString())!), size)
+        case "$": return parseBulkString(from: buffer)
+        case "*": return parseArray(from: buffer)
+        default: return (RESPData.Null, 0)
+        }
+    }
+    
+    func parseBulkString(from buffer: Data) -> (RESPData, Int) {
+        let (payload, bytesConsumed) = readUntilCrlf(from: buffer)
+        guard let payload else {
+            return (.Null, 0)
+        }
+        
+        guard let bulkStringLength = Int(payload.toString()) else {
+            return (.Null, 0)
+        }
+        
+        if bulkStringLength == -1 {
+            return (.Null, 0)
+        }
+        
+        let endOfBulkStringIndex = bytesConsumed + bulkStringLength
+        if endOfBulkStringIndex >= buffer.count {
+            return (.Null, 0)
+        }
+        
+        return (.BulkString(Data(buffer[bytesConsumed..<endOfBulkStringIndex]).toString()), endOfBulkStringIndex+2)
+    }
+    
+    func parseArray(from buffer: Data) -> (RESPData, Int) {
+        var (payload, bytesConsumed) = readUntilCrlf(from: buffer)
+        guard let payload else {
+            return (.Null, 0)
+        }
+        
+        guard let arrayLength = Int(payload.toString()) else {
+            return (.Null, 0)
+        }
+        
+        if arrayLength == -1 {
+            return (.Null, 0)
+        }
+        
+        if arrayLength == 0 {
+            return (.Array([]), bytesConsumed)
+        }
+        
+        var array: [RESPData] = []
+        for _ in 0..<arrayLength {
+            let (arrayItem, len) = extractFrame(from: Data(buffer[bytesConsumed...]))
+            
+            if arrayItem == .Null {
+                return (.Null, 0)
+            }
+            
+            bytesConsumed += len
+            array.append(arrayItem)
+        }
+        
+        return (.Array(array), bytesConsumed)
+    }
+    
+    func readUntilCrlf(from buffer: Data) -> (Data?, Int) {
+        var data: Data = Data()
+        for i in 2..<buffer.count {
+            if buffer[i-1] == "\r" && buffer[i] == "\n" {
+                return (data, i+1)
+            }
+            data.append(buffer[i-1])
+        }
+        return (nil, 0)
+    }
+}
+
+enum RESPData: Equatable {
+    case SimpleString(String)
+    case Error(String)
+    case Integer(Int)
+    case BulkString(String)
+    case Array([RESPData])
+    case Null
+}
+
+extension UInt8 {
+    func toChar() -> Character {
+        Character(UnicodeScalar(self))
+    }
+    
+    static func ==(lhs: UInt8, rhs: Character) -> Bool {
+        lhs.toChar() == rhs
+    }
+}
+
+extension Data {
+    func toString() -> String {
+        String(decoding: self, as: UTF8.self)
+    }
+}
--- Tests/ProtocolTests.swift
+++ Tests/ProtocolTests.swift
@@ -1,3 +1,51 @@
-import XCTest
+import Testing
+@testable import SwiftRedis
 
-
+// generally recommended that a Swift structure or actor be used instead
+// of a class because it allows the Swift compiler to better-enforce concurrency safety.
+struct ProtocolTests {
+    
+    @Test(arguments: [
+        // Test invalid Message
+        ("PING\r\n", (RESPData.Null, 0)),
+        
+        // Test cases for Simple Strings
+        ("+Par",         (.Null, 0)),
+        ("+OK\r\n",      (.SimpleString("OK"), 5)),
+        ("+OK\r\n+Next", (.SimpleString("OK"), 5)),
+        
+        // Test cases for Errors
+        ("-Err",                     (.Null, 0)),
+        ("-Error Message\r\n",       (.Error("Error Message"), 16)),
+        ("-Error Message\r\n+Other", (.Error("Error Message"), 16)),
+        
+        // Test cases for Integers
+        (":10",         (.Null, 0)),
+        (":100\r\n",    (.Integer(100), 6)),
+        (":100\r\n+OK", (.Integer(100), 6)),
+        
+        // Test cases for Bulk Strings
+        ("$5\r\nHel",                   (.Null, 0)),
+        ("$5\r\nHello\r\n",             (.BulkString("Hello"), 11)),
+        ("$12\r\nHello, World\r\n",     (.BulkString("Hello, World"), 19)),
+        ("$12\r\nHello\r\nWorld\r\n",   (.BulkString("Hello\r\nWorld"), 19)),
+        ("$0\r\n\r\n",                  (.BulkString(""), 6)),
+        ("$-1\r\n",                     (.Null, 0)),
+        
+        // Test case for Arrays
+        ("*0",                          (.Null, 0)),
+        ("*0\r\n",                      (.Array([]), 4)),
+        ("*-1\r\n",                     (.Null, 0)),
+        ("*2\r\n$5\r\nhello\r\n$5\r\n", (.Null, 0)),
+        
+        ("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n",    (.Array([.BulkString("hello"), .BulkString("world")]), 26)),
+        ("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n+OK", (.Array([.BulkString("hello"), .BulkString("world")]), 26)),
+        ("*3\r\n:1\r\n:2\r\n:3\r\n",                (.Array([.Integer(1), .Integer(2), .Integer(3)]), 16)),
+        ("*3\r\n:1\r\n:2\r\n:3\r\n+OK",             (.Array([.Integer(1), .Integer(2), .Integer(3)]), 16))
+        
+    ]) func testReadFrameSimpleString(input: String, expected: (RESPData, Int)) {
+        let buffer = input.data(using: .utf8)!
+        let actual = Protocol().extractFrame(from: buffer)
+        #expect(actual == expected)
+    }
+}
